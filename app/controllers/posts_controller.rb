875cb1ef6fce79690bedfdf5baf529c0384bdf5a
Vedant <vshetty.vs@gmail.com>
2020-02-02 12:16:02 +0530

    Added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -8,7 +9,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def create
@@ -22,11 +22,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(whitelisted_post_params)
       redirect_to @post
     else
@@ -35,9 +33,7 @@ class PostsController < ApplicationController
   end
 
   def destroy 
-    @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to posts_path
   end
 
@@ -45,5 +41,9 @@ class PostsController < ApplicationController
 
   def whitelisted_post_params
     params.require(:post).permit(:title,:content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
